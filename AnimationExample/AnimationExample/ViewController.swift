b50bac807bfd6ef540cf5d9e7c81c79611f5215e
Sayat <102688997+Kalnazar@users.noreply.github.com>
2022-04-05 14:07:21 +0600

    Add files via upload

---
+++ AnimationExample/AnimationExample/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  AnimationExample
+//
+//  Created by Аршат Талап on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var targetImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func animatePressed(_ sender: UIButton) {
+        switch sender.tag{
+        case 0:
+            UIView.animate(withDuration: 1){
+                self.targetImage.alpha = 0
+            }
+        case 1:
+            UIView.animate(withDuration: 1){
+                self.targetImage.alpha = 1
+            }
+        case 2:
+            UIView.animate(withDuration: 1){
+                self.targetImage.center = CGPoint(x: self.view.bounds.width, y: 0)
+            }
+        case 3:
+            UIView.animate(withDuration: 1){
+                self.targetImage.center = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height/2)
+            }
+        case 4:
+            UIView.animate(withDuration: 1){
+                self.targetImage.transform = CGAffineTransform(scaleX: 2, y: 2)
+            }
+        case 5:
+            UIView.animate(withDuration: 1){
+                self.targetImage.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }
+        default:
+            break
+        }
+    }
+}
+
